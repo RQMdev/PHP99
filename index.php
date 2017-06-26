395812901950a4da571260633eefec70f976bd8d
Benoit Laurent <benoit.jacques.felix@gmail.com>
2017-06-26 22:52:31 +0200

    Change return/print and delete the level 3 :)

--- index.php
+++ index.php
@@ -17,23 +17,31 @@
     <span><h1>Exercice PHP sur la manipulation<br> de Tableaux</h1></span>
     <section class="section1 is-showing">
       <h2>Exercice 1 Niveau 2</h2>
-      <?php  
+      <?php
+
         $viandes = array("poulet", "boeuf", "mouton");
         $cuissons = array("à point", "saignant", "bleu", "bien cuit");
         $commandes = array();
 
+
         //One Loop Try
         function commander($array1, $array2){
+
           for ($i = 0; $i < count($array1)*count($array2); $i++){
+
             $commandes[] = $array1[$i%count($array1)].' '.$array2[$i/count($array1)];
           }
-          print "Voici la liste des commandes :";
-          print "<pre>";
-          print_r($commandes);
-          print "</pre>";
+
+          return $commandes;
         }
-        commander($viandes, $cuissons);
-        
+
+
+        print "Voici la liste des commandes :";
+        print "<pre>";
+        print_r(commander($viandes, $cuissons));
+        print "</pre>";
+
+
       ?>
     </section>
     <section class="section2">
@@ -43,13 +51,15 @@
         
         function explod_e($array){
 
-        print "Voici le tableau contenant les mots séparés :";
-        print "<pre>";
-        print_r(explode(" ", $array));
-        print "</pre>";
+          return explode(" ", $array);
+        
         }
 
-        explod_e($genres);
+
+        print "Voici le tableau contenant les mots séparés :";
+        print "<pre>";
+        print_r(explod_e($genres));
+        print "</pre>";
         
       ?>
     </section>
@@ -60,17 +70,23 @@
 
         function itTakesTwo($array){
           $fruits_favoris = [];
+
           for ($i = 0; $i < 2; $i++){
+
             array_push($fruits_favoris, $array[count($array)-1]);
             array_pop($array);
+
           } 
-            print "Voici les deux derniers éléments sortis du tableau :";
-            print "<pre>";
-            print_r($fruits_favoris);
-            print "</pre>";
+            
+            return $fruits_favoris;
+            
         }
 
-        itTakesTwo($fruits);
+
+        print "Voici les deux derniers éléments sortis du tableau :";
+        print "<pre>";
+        print_r(itTakesTwo($fruits));
+        print "</pre>";
 
       ?>
     </section>
@@ -92,14 +108,17 @@
             }
 
           array_pop($array);
-          print "Le tableau sans sa valeur du milieu est :";
-          print "<pre>";
-          print_r($array);
-          print "</pre>";
+
+          return $array;
+          
           }
         }
 
-        chooseYourSide($prenoms);
+
+        print "Le tableau sans sa valeur du milieu est :";
+        print "<pre>";
+        print_r(chooseYourSide($prenoms));
+        print "</pre>";
 
       ?>
     </section>
@@ -113,18 +132,21 @@
           $holder = "";
 
           foreach($array as $keys => $value){
+
             if (strlen($value) > strlen($holder)){
               $holder = $value;
             }
           }
 
-          print "La chaine de caractère la plus longue est :";
-          print "<pre>";
-          print_r($holder);
-          print "</pre>";
+          return $holder;
+          
         }
 
-        findTheLongest($phrase);
+
+        print "La chaine de caractère la plus longue est :";
+        print "<pre>";
+        print_r(findTheLongest($phrase));
+        print "</pre>";
 
       ?>
     </section>
@@ -134,18 +156,21 @@
         $portables = ["0612459623", "0645896235", "0678145263", "0647895263", "0655069912"];
 
         function convertToInternational($array){
+
           for ($i = 0; $i < count($array); $i++){
+            
             $array[$i] = substr($array[$i], 1);
             $array[$i] = "+33".$array[$i];
           }
-
-          print "Les numéros internationaux sont :";
-          print "<pre>";
-          print_r($array);
-          print "</pre>";
+          
+          return $array;
+          
         }
 
-        convertToInternational($portables);
+        print "Les numéros internationaux sont :";
+        print "<pre>";
+        print_r(convertToInternational($portables));
+        print "</pre>";
 
       ?>
     </section>
@@ -155,20 +180,25 @@
         $sadisme = ["haha", "hehe", "j'aime mon travail", "une vraie joie", "je changerai pour rien au monde", "hhhhhhhh", "have fun !"];
 
         function howManyHThereIs($array){
+
           $global_count = 0;
+
           foreach ($array as $keys => $value){
+
             $global_count += substr_count($value, "h");
           }
-          print "Il y a ";
+          
           print_r($global_count);
-          print " fois la lettre H !";
+          
         }
 
-        howManyHThereIs($sadisme);
+        print "Il y a ";
+        print_r(howManyHThereIs($sadisme));
+        print " fois la lettre H !";
         
       ?>
     </section>
     <script type="text/javascript" src="jquery-3.2.1.slim.js"></script>
-    <script type="text/javascript" src="learn-jquery.js"></script>
+    <script type="text/javascript" src="main.js"></script>
   </body>
 </html>
--- jquery_instruction.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-
-    $('.button').before('<input type="text" name="array1_3"><br>');
-    
-});
